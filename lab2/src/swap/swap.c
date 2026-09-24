e7df47caf7bc8562f354e7ffc5eec09dc063d0fd
innanovak41-a11y <innanovak41@gmail.com>
2026-09-24 14:07:29 +0000

    lr2

--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -1,6 +1,7 @@
 #include "swap.h"
 
-void Swap(char *left, char *right)
-{
-	// ваш код здесь
+void Swap(char *left, char *right) {
+    char temp = *left;   // сохраняем значение, на которое указывает left
+    *left = *right;      // в left записываем значение из right
+    *right = temp;       // в right записываем сохранённое значение
 }
